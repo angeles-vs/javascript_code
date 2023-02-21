2bd177c708f83da52f406bd5c4b3a35e22c295a6
Angeles <angelessevilla18@gmail.com>
2023-02-21 16:22:30 +0100

    Add actualizacion

--- Juego/game.js
+++ Juego/game.js
@@ -4,8 +4,25 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame() {
-    game.fillRect(0,0,100,100);
-}
+   let canvasSize;
+   
+   if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+   } else {
+    canvasSize = window. innerHeight * 0.8;
+   }
+   
+canvas.setAttribute('width', canvasSize);
+canvas.setAttribute('height', canvasSize);
 
-window.innerHeight
-window.innerWidth
+const elementsSize= canvasSize / 10;
+
+console.log({ canvasSize, elementsSize })
+
+game.font = elementsSize + 'px Verdana';
+game.textAlign = 'end';
+
+for (let i = 1; i <= 10; i++){
+    game.fillText(emojis['X'], elementsSize * i, elementsSize);
+}
+}
